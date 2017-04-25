6cc1a5e6b8611d8163bb56696aae8909abaed250
TheDurker <thedurker@gmail.com>
2017-04-25 15:17:21 +0200

    Stats and effects

---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/Effect.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Dungeon_Crawler_2D
+{
+    class Effect
+    {
+        TextureManager textures;
+        Stats stats;
+        public Effects effect;
+        public int timer, power;
+        
+        public Effect(TextureManager textures, Effects effect, int timer, Stats stats, int power)
+        {
+            this.textures = textures;
+            this.stats = stats;
+            this.timer = timer;
+            this.effect = effect;
+            this.power = power;
+        }
+
+        // the effect of the effect happens
+        public void Update()
+        {
+            if (timer < 0)
+            {
+                if (effect == Effects.poison)
+                {
+                    stats.AddStats(-power, Stat.health);
+                }
+                if (effect == Effects.bleed)
+                {
+
+                }
+            }
+        }
+        
+        // if there is a animation for the effect it will happen here
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/Stats.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler_2D
+{
+    public enum Effects
+    {
+        poison, bleed, confusion, strenghtBuff
+    }
+    public enum Stat
+    {
+        maxHealth, health, maxMana, mana, strength, inteligence, dextarity, luck
+    }
+
+    class Stats
+    {
+        
+        
+
+        int maxHealth, health, maxMana, mana, strength, inteligence, dextarity, luck;
+        List<Effect> activeEffects;
+        TextureManager textures;
+
+        public Stats(TextureManager textures, int maxHealth, int maxMana, int strength, int inteligence, int dextarity, int luck)
+        {
+            this.textures = textures;
+            this.health = maxHealth;
+            maxHealth = health;
+            this.maxMana = maxMana;
+            mana = maxMana;
+            this.strength = strength;
+            this.inteligence = inteligence;
+            this.dextarity = dextarity;
+            this.luck = luck;
+            activeEffects = new List<Effect>(4);//number goes up 
+        }
+
+        public void AddStats(int addition, Stat stat)
+        {
+            if (stat == Stat.maxHealth)
+            {
+                maxHealth += addition;
+            }
+            if (stat == Stat.health)
+            {
+                health += addition;
+            }
+            if (stat == Stat.maxMana)
+            {
+                maxMana += addition;
+            }
+            if (stat == Stat.mana)
+            {
+                mana += addition;
+            }
+            if (stat == Stat.strength)
+            {
+                strength += addition;
+            }
+            if (stat == Stat.inteligence)
+            {
+                inteligence += addition;
+            }
+            if (stat == Stat.dextarity)
+            {
+                dextarity += addition;
+            }
+            if (stat == Stat.luck)
+            {
+                luck += addition;
+            }
+        }
+
+        public void AddEffect(int length, Effects effect, int power)
+        {
+            if (CheckEffects(effect) == false)
+            {
+                activeEffects.Add(new Effect(textures, effect, length, this, power));
+            }
+            else
+            {
+                foreach (Effect e in activeEffects)
+                {
+                    if (e.effect == effect)
+                    {
+                        e.timer = length;
+                        e.power = power;
+                    }
+                }
+            }
+
+            
+        }
+
+        public bool CheckEffects(Effects effect)
+        {
+            foreach(Effect e in activeEffects)
+            {
+                if (e.effect == effect)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Buff(int multiplication, Stat stat)
+        {
+            if (stat == Stat.health)
+            {
+                health *= multiplication;
+            }
+            if (stat == Stat.mana)
+            {
+                mana *= multiplication;
+            }
+            if (stat == Stat.strength)
+            {
+                strength *= multiplication;
+            }
+            if (stat == Stat.inteligence)
+            {
+                inteligence *= multiplication;
+            }
+            if (stat == Stat.dextarity)
+            {
+                dextarity *= multiplication;
+            }
+            if (stat == Stat.luck)
+            {
+                luck *= multiplication;
+            }
+        }
+    }
+}
