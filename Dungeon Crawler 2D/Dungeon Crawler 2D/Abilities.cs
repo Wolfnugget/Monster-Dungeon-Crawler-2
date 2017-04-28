914c041260b0e28d2cbcdc19d30b83e99f10cbf4
TheDurker <thedurker@gmail.com>
2017-04-28 14:12:36 +0200

    Enemy and Abilities classes
    Kommer behövas att ändra samt balansera, samt kallas till från andra
    klasser, men det kommer när combat klasserna är skapade

---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/Abilities.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler_2D
+{
+    public enum UsedBy
+    {
+        player, enemy
+    }
+
+    public enum UsedAbility
+    {
+        Hit, Defence, Magic, Dodge, miss//osv
+    }
+
+    class Abilities
+    {
+        public UsedAbility usedAbility;
+        private UsedBy usedBy;
+        public int power;
+        private Random rand = new Random();
+
+        public Abilities(UsedBy usedBy)
+        {
+            this.usedBy = usedBy;
+        }
+
+
+        public void Hit(Enemy enemy, Object.Player player)
+        {
+            if (usedBy == UsedBy.player)
+            {
+                int accuracy = player.stats.CheckStat(Stat.dexterity) + (player.stats.CheckStat(Stat.luck) / 2) + rand.Next(0, 100);
+                if (accuracy >= 50)
+                {
+                    usedAbility = UsedAbility.Hit;
+                    if (accuracy >= 100)
+                    {
+                        power = player.stats.CheckStat(Stat.strength);
+                        enemy.stats.AddEffect(3, Effects.bleed, 1);
+                    }
+                    else
+                    {
+                        power = player.stats.CheckStat(Stat.strength);
+                    }
+                }
+                else { usedAbility = UsedAbility.miss; power = 0; }
+            }
+            else
+            {
+                int accuracy = enemy.stats.CheckStat(Stat.dexterity) + (enemy.stats.CheckStat(Stat.luck) / 2) + rand.Next(0, 100);
+                if (accuracy >= 50)
+                {
+                    usedAbility = UsedAbility.Hit;
+                    if (accuracy >= 100)
+                    {
+                        power = enemy.stats.CheckStat(Stat.strength) * 2;
+                        player.stats.AddEffect(3, Effects.bleed, 1);
+                    }
+                    else
+                    {
+                        power = enemy.stats.CheckStat(Stat.strength);
+                    }
+                }
+                else { usedAbility = UsedAbility.miss; power = 0; }
+            }
+        }
+
+        public void Defence(Enemy enemy, Object.Player player)
+        {
+            usedAbility = UsedAbility.Defence;
+            if (usedBy == UsedBy.player)
+            {
+                power = (player.stats.CheckStat(Stat.strength) / 2) + (player.stats.CheckStat(Stat.dexterity) / 2);
+            }
+            else
+            {
+                power = (enemy.stats.CheckStat(Stat.strength) / 2) + (enemy.stats.CheckStat(Stat.dexterity) / 2);
+            }
+        }
+
+
+        public void Magic(Enemy enemy, Object.Player player)
+        {
+            if (usedBy == UsedBy.player)
+            {
+                int accuracy = player.stats.CheckStat(Stat.dexterity) + (player.stats.CheckStat(Stat.luck) / 2) + rand.Next(0, 80);
+                if (accuracy >= 50)
+                {
+                    usedAbility = UsedAbility.Magic;
+                    if (accuracy >= 100)
+                    {
+                        power = player.stats.CheckStat(Stat.inteligence) + player.stats.CheckStat(Stat.luck);
+                        enemy.stats.AddEffect(3, Effects.confusion, 1);
+                    }
+                    else
+                    {
+                        power = player.stats.CheckStat(Stat.inteligence) + player.stats.CheckStat(Stat.luck);
+                    }
+                }
+                else { usedAbility = UsedAbility.miss; power = 0; }
+            }
+            else
+            {
+                int accuracy = enemy.stats.CheckStat(Stat.dexterity) + (enemy.stats.CheckStat(Stat.luck) / 2) + rand.Next(0, 100);
+                if (accuracy >= 50)
+                {
+                    usedAbility = UsedAbility.Magic;
+                    if (accuracy >= 100)
+                    {
+                        power = enemy.stats.CheckStat(Stat.inteligence) + enemy.stats.CheckStat(Stat.luck);
+                        enemy.stats.AddEffect(3, Effects.confusion, 1);
+                    }
+                    else
+                    {
+                        power = enemy.stats.CheckStat(Stat.inteligence) + enemy.stats.CheckStat(Stat.luck);
+                    }
+                }
+                else { usedAbility = UsedAbility.miss; power = 0; }
+            }
+        }
+
+
+        public void Dodge(Enemy enemy, Object.Player player)
+        {
+            if (usedBy == UsedBy.player)
+            {
+                int accuracy = player.stats.CheckStat(Stat.dexterity) + (player.stats.CheckStat(Stat.luck)) + rand.Next(0, 80);
+                if (accuracy >= 50)
+                {
+                    usedAbility = UsedAbility.Dodge;
+                }
+                else usedAbility = UsedAbility.miss;
+            }
+            else
+            {
+                int accuracy = enemy.stats.CheckStat(Stat.dexterity) + (enemy.stats.CheckStat(Stat.luck)) + rand.Next(0, 80);
+                if (accuracy >= 50)
+                {
+                    usedAbility = UsedAbility.Dodge;
+                }
+                else usedAbility = UsedAbility.miss;
+            }
+        }
+
+        public void PoisonHit(Enemy enemy, Object.Player player)
+        {
+            if (usedBy == UsedBy.player)
+            {
+                int accuracy = player.stats.CheckStat(Stat.dexterity) + (player.stats.CheckStat(Stat.luck) / 2) + rand.Next(0, 100);
+                if (accuracy >= 50)
+                {
+                    usedAbility = UsedAbility.Hit;
+                    if (accuracy >= 100)
+                    {
+                        power = player.stats.CheckStat(Stat.strength);
+                        enemy.stats.AddEffect(3, Effects.poison, 1);
+                    }
+                    else
+                    {
+                        power = player.stats.CheckStat(Stat.strength) / 2;
+                        enemy.stats.AddEffect(3, Effects.poison, 1);
+                    }
+                }
+                else { usedAbility = UsedAbility.miss; power = 0; }
+            }
+            else
+            {
+                int accuracy = enemy.stats.CheckStat(Stat.dexterity) + (enemy.stats.CheckStat(Stat.luck) / 2) + rand.Next(0, 100);
+                if (accuracy >= 50)
+                {
+                    usedAbility = UsedAbility.Hit;
+                    if (accuracy >= 100)
+                    {
+                        power = enemy.stats.CheckStat(Stat.strength);
+                        player.stats.AddEffect(3, Effects.poison, 1);
+                    }
+                    else
+                    {
+                        power = enemy.stats.CheckStat(Stat.strength) / 2;
+                        player.stats.AddEffect(3, Effects.poison, 1);
+                    }
+                }
+                else { usedAbility = UsedAbility.miss; power = 0; }
+            }
+        }
+    }
+}
+
---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace Dungeon_Crawler_2D
+{
+    public enum EnemyType
+    {
+        zombie
+    }
+
+    class Enemy
+    {
+        TextureManager textures;
+        public Stats stats;
+        Random rand = new Random();
+        public Abilities ability;
+        Object.Player player;
+        EnemyType theEnemy;
+
+        public Enemy(TextureManager textures, EnemyType theEnemy, Object.Player player)
+        {
+            this.textures = textures;
+            this.theEnemy = theEnemy;
+            this.player = player;
+            GiveStats();
+        }
+
+        public void Update()
+        {
+            if (theEnemy == EnemyType.zombie)
+            {
+                if (rand.Next(0,2) == 1)
+                {
+                    ability.Hit(this, player);
+                }
+                else
+                {
+                    ability.Defence(this, player);
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public void GiveStats()
+        {
+            if (theEnemy == EnemyType.zombie)
+            {
+                int health = rand.Next(100, 150) + 20 * player.stats.CheckStat(Stat.level);
+                int mana = rand.Next(100, 150) + 10 * player.stats.CheckStat(Stat.level);
+
+                int strenght = rand.Next(6, 15) + 3 * player.stats.CheckStat(Stat.level);
+                int inteligence = rand.Next(6, 15) + 3 * player.stats.CheckStat(Stat.level);
+                int dextarity = rand.Next(6, 15) + 3 * player.stats.CheckStat(Stat.level);
+                int luck = rand.Next(6, 15) + 3 * player.stats.CheckStat(Stat.level);
+                stats = new Stats(textures, health, health, mana, mana, strenght, inteligence, dextarity, luck, 0, 0, 0);
+            }
+        }
+        
+    }
+}
