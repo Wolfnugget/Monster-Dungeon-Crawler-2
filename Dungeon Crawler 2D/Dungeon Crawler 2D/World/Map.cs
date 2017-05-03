a331ce188d973d20c71adc4a827ba4228c548cd2
The Durker <thedurker@gmail.com>
2017-05-03 22:31:38 +0200

    small change to loading boss

--- Dungeon Crawler 2D/Dungeon Crawler 2D/World/Map.cs
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/World/Map.cs
@@ -113,7 +113,7 @@ namespace Dungeon_Crawler_2D.World
                     OnEvent(args);
                 }
             }
-            else if (type == TileType.ExitPortal)
+            else if (type == TileType.boss)
             {
                 MapEventArgs args = new MapEventArgs(MapEventType.StartCombat);
                 args.enemy = EnemyType.boss;
