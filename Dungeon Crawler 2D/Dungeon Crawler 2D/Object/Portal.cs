bde7149e4a12ab508922f6ade2d45f4bf654f3e6
Wolfnugget <john@bluegalaxy.se>
2017-05-03 22:37:21 +0200

    fil va ignored

---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/Object/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Dungeon_Crawler_2D.Object
+{
+    class Portal: Object
+    {
+        bool active;
+
+        public Portal(Texture2D texture, Vector2 position, bool active)
+            : base(texture, position)
+        {
+            this.active = active;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
