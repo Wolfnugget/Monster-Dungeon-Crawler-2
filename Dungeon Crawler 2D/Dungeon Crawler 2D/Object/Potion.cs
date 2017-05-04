7f5efdd250aa306d7f9e400c654a77ea263a3af3
Wolfnugget <john@bluegalaxy.se>
2017-05-04 12:11:41 +0200

    Potions

---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/Object/Potion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Dungeon_Crawler_2D.Object
+{
+    class Potion: Object
+    {
+        Vector2 origin;
+
+        public Potion(Texture2D texture, Vector2 position)
+            : base(texture, position)
+        {
+            origin = new Vector2(texture.Width / 2, texture.Height / 2);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, new Rectangle(0, 0, texture.Width, texture.Height), Color.White, 0, origin, 1, SpriteEffects.None, 1);
+        }
+    }
+}
