34d7edc43f8ee2463027937bc3d6fd29f14aa2c2
Wolfnugget <john@bluegalaxy.se>
2017-05-04 09:44:52 +0200

    Saker blev ignorerade igen

---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/Object/Animated.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler_2D.Object
+{
+    public abstract class Animated: Object
+    {
+
+        protected Point startingFrame, frame, frames, frameSize;
+        float frameTime, frameDuration;
+        Vector2 origin;
+        protected SpriteEffects effect;
+
+        private Rectangle srcRec
+        {
+            get
+            {
+                return new Rectangle(frame.X * frameSize.Y,
+                    frame.Y * frameSize.Y,
+                    frameSize.X, frameSize.Y);
+            }
+        }
+
+        public Animated(Texture2D texture, Vector2 position, Point frameSize, Point frames, float frameTime)
+            : base(texture, position)
+        {
+            this.position = position;
+            this.frameSize = frameSize;
+            this.frameTime = frameTime;
+            frame = startingFrame;
+            this.frames = frames;
+
+            origin = new Vector2(frameSize.X / 2, frameSize.Y / 2);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Animate(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, srcRec, Color.White, 0, origin, 1, effect, 1);
+        }
+
+        protected void Animate(GameTime gameTime)
+        {
+            frameDuration -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (frame.Y < startingFrame.Y || frame.Y > startingFrame.Y + frames.Y)
+            {
+                frame.Y = startingFrame.Y;
+            }
+            if (frame.X < startingFrame.X || frame.X > startingFrame.X + frames.X)
+            {
+                frame.X = startingFrame.X;
+            }
+            if (frameDuration <= 0)
+            {
+                frameDuration = frameTime;
+                if (frame.X < startingFrame.X + frames.X)
+                {
+                    frame.X++;
+                }
+                else if (frame.Y < startingFrame.Y + frames.Y)
+                {
+                    frame.X = startingFrame.X;
+                    frame.Y++;
+                }
+                else
+                {
+                    frame = startingFrame;
+                }
+            }
+        }
+    }
+}
---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/World/GameWorld.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+
+namespace Dungeon_Crawler_2D.World
+{
+    class GameWorld: Map
+    {
+        public GameWorld(TextureManager textures, ContentManager content)
+            : base(textures, content)
+        {
+
+            currentLocation = Location.Overworld;
+            rooms.Add(Location.Overworld, new PreMadeArea("Maps/Overworld/Main.txt", textures, content));
+        }
+
+        protected override void ChangeArea(TileType entrance)
+        {
+            MapEventArgs args = new MapEventArgs(MapEventType.ChangeRoom);
+
+            if (currentLocation == Location.Overworld)
+            {
+                Point dimensions = new Point(rand.Next(80, 120), rand.Next(80, 120));
+                currentLocation = Location.Dungeon;
+                if (rooms.ContainsKey(Location.Dungeon))
+                {
+                    rooms[Location.Dungeon] = new GeneratedDungeon(dimensions, textures, content);
+                }
+                else
+                {
+                    rooms.Add(Location.Dungeon, new GeneratedDungeon(dimensions, textures, content));
+                }
+            }
+            else
+            {
+                currentLocation = Location.Overworld;
+            }
+            args.Position = rooms[currentLocation].GetTileCenterOfType(entrance);
+            OnEvent(args);
+        }
+    }
+}
