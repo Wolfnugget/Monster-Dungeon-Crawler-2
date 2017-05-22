4ef6b738c89cfc5d56b61bf7afa0fed45e20eaee
JAlfredS <alfred.bengtsson92@gmail.com>
2017-05-22 23:44:14 +0200

    Github ignored but creditscreen added

---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/Menus/CreditScreen.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Dungeon_Crawler_2D.Menus
+{
+    public class CreditScreen : GameScreen
+    {
+        List<FadeAnimation> fade;
+        List<Texture2D> images;
+
+        FileManager fileManager;
+        int imageNumber;
+
+        public override void LoadContent(ContentManager Content, InputManager inputManager, GraphicsDevice graphicsDevice)
+        {
+            base.LoadContent(Content, inputManager, graphicsDevice);
+
+            imageNumber = 0;
+            fileManager = new FileManager();
+            fade = new List<FadeAnimation>();
+            images = new List<Texture2D>();
+
+            fileManager.LoadContent("Menus/Load/CreditText.txt", attributes, contents);
+
+            inputManager = new InputManager();
+
+            for (int i = 0; i < attributes.Count; i++)
+            {
+                for (int j = 0; j < attributes[i].Count; j++)
+                {
+                    switch (attributes[i][j])
+                    {
+                        case "Image":
+                            images.Add(content.Load<Texture2D>(contents[i][j]));
+                            fade.Add(new FadeAnimation());
+                            break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < attributes.Count; i++)
+            {
+                fade[i].LoadContent(content, images[i], "", new Vector2(ScreenManager.Instance.Dimensions.X / 2 - images[i].Width / 2, ScreenManager.Instance.Dimensions.Y / 2 - images[i].Height / 2));
+                fade[i].Scale = 1.0f;
+                fade[i].IsActive = false;
+            }
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+            fileManager = null;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            inputManager.Update();
+
+            fade[imageNumber].Update(gameTime);
+
+           
+            if (inputManager.KeyPressed(Keys.Enter))
+            {
+                ScreenManager.Instance.AddScreen(new TitleScreen(), inputManager, fade[imageNumber].Alpha);
+            }
+
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            fade[imageNumber].Draw(spriteBatch);
+        }
+
+
+    }
+}
