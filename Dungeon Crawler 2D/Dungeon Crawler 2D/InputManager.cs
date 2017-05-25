2c0b47f8de2bb4ae6695408328ed56b25c812187
Wolfnugget <john@bluegalaxy.se>
2017-05-25 15:22:08 +0200

    InputManager flytad och gjord till statisk

---
+++ Dungeon Crawler 2D/Dungeon Crawler 2D/InputManager.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Crawler_2D
+{
+    public static class InputManager
+    {
+        static KeyboardState prevKeyState, keyState;
+
+        public static KeyboardState PrevKeyState
+        {
+            get { return prevKeyState; }
+            set { prevKeyState = value; }
+        }
+
+        public static KeyboardState KeyState
+        {
+            get { return keyState; }
+            set { keyState = value; }
+        }
+
+        public static void Update()
+        {
+            prevKeyState = keyState;
+            keyState = Keyboard.GetState();
+        }
+
+        public static bool KeyPressed(Keys key)
+        {
+            if (keyState.IsKeyDown(key) && prevKeyState.IsKeyUp(key))
+                return true;
+            return false;
+        }
+
+        public static bool KeyPressed(params Keys[] keys)
+        {
+            foreach (Keys key in keys)
+            {
+                if (keyState.IsKeyDown(key) && prevKeyState.IsKeyUp(key))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool KeyReleased(Keys key)
+        {
+            if (keyState.IsKeyUp(key) && prevKeyState.IsKeyDown(key))
+                return true;
+            return false;
+        }
+
+        public static bool KeyReleased(params Keys[] keys)
+        {
+            foreach (Keys key in keys)
+            {
+                if (keyState.IsKeyUp(key) && prevKeyState.IsKeyDown(key))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool KeyDown(Keys key)
+        {
+            if (keyState.IsKeyDown(key))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool KeyDown(params Keys[] keys)
+        {
+            foreach (Keys key in keys)
+            {
+                if (keyState.IsKeyDown(key))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
